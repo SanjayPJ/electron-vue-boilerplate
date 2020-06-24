fe715967cd3ee88558474d254af6c5d127b39aa1
Sanjay PJ <sanjaypjayan2000@gmail.com>
2020-06-24 11:50:07 +0530

    clean up completed

--- src/app.js
+++ src/app.js
@@ -6,13 +6,11 @@ import App from './App.vue'
 Vue.use(VueRouter)
 
 import Contacts from './components/Contacts.vue'
-import Music from './components/Music.vue'
 
 const router = new VueRouter({
   routes: [
     { path: '/', component: App },
     { path: '/contacts', component: Contacts },
-    { path: '/music', component: Music }
   ]
 })
 
